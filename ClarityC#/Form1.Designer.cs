04cf94d4edafe7995aa896a14dffbbbec734e4ac
DroteC C <doshiracklkkk@mail.ru>
2025-03-15 18:56:14 +0500

    #

--- ClarityC#/Form1.Designer.cs
+++ ClarityC#/Form1.Designer.cs
@@ -28,32 +28,102 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            ReceiptButn = new Button();
+            SettingButn = new Button();
+            MusikButn = new Button();
+            ClockButn = new Button();
+            groupBox1 = new GroupBox();
+            groupBox1.SuspendLayout();
             SuspendLayout();
             // 
-            // button1
+            // ReceiptButn
             // 
-            button1.Location = new Point(148, 113);
-            button1.Name = "button1";
-            button1.Size = new Size(173, 75);
-            button1.TabIndex = 0;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
+            ReceiptButn.BackColor = SystemColors.GradientActiveCaption;
+            ReceiptButn.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            ReceiptButn.Location = new Point(6, 143);
+            ReceiptButn.Name = "ReceiptButn";
+            ReceiptButn.Size = new Size(133, 65);
+            ReceiptButn.TabIndex = 0;
+            ReceiptButn.TabStop = false;
+            ReceiptButn.Text = "Расписание";
+            ReceiptButn.UseVisualStyleBackColor = false;
+            ReceiptButn.Click += button1_Click;
+            // 
+            // SettingButn
+            // 
+            SettingButn.BackColor = SystemColors.GradientActiveCaption;
+            SettingButn.Location = new Point(6, 445);
+            SettingButn.Name = "SettingButn";
+            SettingButn.Size = new Size(133, 65);
+            SettingButn.TabIndex = 0;
+            SettingButn.TabStop = false;
+            SettingButn.Text = "Настройки";
+            SettingButn.UseVisualStyleBackColor = false;
+            // 
+            // MusikButn
+            // 
+            MusikButn.BackColor = SystemColors.GradientActiveCaption;
+            MusikButn.Location = new Point(6, 293);
+            MusikButn.Name = "MusikButn";
+            MusikButn.Size = new Size(133, 65);
+            MusikButn.TabIndex = 0;
+            MusikButn.TabStop = false;
+            MusikButn.Text = "Музыка";
+            MusikButn.UseVisualStyleBackColor = false;
+            // 
+            // ClockButn
+            // 
+            ClockButn.BackColor = SystemColors.GradientActiveCaption;
+            ClockButn.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            ClockButn.Location = new Point(6, 22);
+            ClockButn.Name = "ClockButn";
+            ClockButn.Size = new Size(133, 65);
+            ClockButn.TabIndex = 1;
+            ClockButn.TabStop = false;
+            ClockButn.Text = "Будильник";
+            ClockButn.UseVisualStyleBackColor = false;
+            // 
+            // groupBox1
+            // 
+            groupBox1.BackColor = SystemColors.ButtonShadow;
+            groupBox1.BackgroundImageLayout = ImageLayout.None;
+            groupBox1.Controls.Add(ClockButn);
+            groupBox1.Controls.Add(ReceiptButn);
+            groupBox1.Controls.Add(SettingButn);
+            groupBox1.Controls.Add(MusikButn);
+            groupBox1.ForeColor = SystemColors.ActiveCaptionText;
+            groupBox1.Location = new Point(-1, -11);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(160, 577);
+            groupBox1.TabIndex = 2;
+            groupBox1.TabStop = false;
+           
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(button1);
+            AutoScaleMode = AutoScaleMode.None;
+            BackColor = SystemColors.AppWorkspace;
+            BackgroundImage = Properties.Resources.fformImage;
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(484, 561);
+            Controls.Add(groupBox1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximumSize = new Size(500, 600);
+            MinimumSize = new Size(500, 600);
             Name = "Form1";
-            Text = "Form1";
+            Text = "Clarity";
+            
+            groupBox1.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
 
-        private Button button1;
+        private Button ReceiptButn;
+        private Button SettingButn;
+        private Button MusikButn;
+        private Button ClockButn;
+        private GroupBox groupBox1;
     }
 }
--- ClarityC#/Form1.cs
+++ ClarityC#/Form1.cs
@@ -2,16 +2,33 @@ namespace ClarityC_
 {
     public partial class Form1 : Form
     {
+        Point movestart;
         public Form1()
         {
             InitializeComponent();
 
-            button1.Click += button1_Click;
+            ReceiptButn.Click += button1_Click;
+            SettingButn.Click += button2_Click;
+            MusikButn.Click += btn3_Click;
+            groupBox1.BackColor = Color.Transparent;
         }
-
+        private void button2_Click(object? sender, EventArgs e)
+        {
+            MusikButn.Visible = !MusikButn.Visible;
+        }
+        private void btn3_Click(object? sender, EventArgs e)
+        {
+            this.BackColor = Color.White;
+        }
         private void button1_Click(object? sender, EventArgs e)
         {
-
+            groupBox1.Visible = !groupBox1.Visible;
         }
+
+       
+
+        
+
+        
     }
 }
