d097bd7989bf3bf1e1e913993b0e78ebe24cb990
DroteC C <doshiracklkkk@mail.ru>
2025-03-20 22:28:01 +0500

    #

--- ClarityC#/Form1.Designer.cs
+++ ClarityC#/Form1.Designer.cs
@@ -34,7 +34,13 @@
             MusikButn = new Button();
             ClockButn = new Button();
             groupBox1 = new GroupBox();
+            button1 = new Button();
+            numericUpDown1 = new NumericUpDown();
+            numericUpDown2 = new NumericUpDown();
+            textBox1 = new TextBox();
             groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2).BeginInit();
             SuspendLayout();
             // 
             // ReceiptButn
@@ -48,7 +54,6 @@
             ReceiptButn.TabStop = false;
             ReceiptButn.Text = "Расписание";
             ReceiptButn.UseVisualStyleBackColor = false;
-            ReceiptButn.Click += button1_Click;
             // 
             // SettingButn
             // 
@@ -102,12 +107,59 @@
             groupBox1.TabIndex = 2;
             groupBox1.TabStop = false;
             // 
+            // button1
+            // 
+            button1.BackColor = Color.Black;
+            button1.Location = new Point(218, 143);
+            button1.Name = "button1";
+            button1.Size = new Size(130, 39);
+            button1.TabIndex = 5;
+            button1.Text = "сохранить";
+            button1.UseVisualStyleBackColor = false;
+            button1.Visible = false;
+            button1.Click += button1_Click_1;
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.BackColor = SystemColors.HotTrack;
+            numericUpDown1.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            numericUpDown1.ForeColor = SystemColors.Menu;
+            numericUpDown1.Location = new Point(218, 61);
+            numericUpDown1.Maximum = new decimal(new int[] { 23, 0, 0, 0 });
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(55, 23);
+            numericUpDown1.TabIndex = 6;
+            numericUpDown1.Visible = false;
+            // 
+            // numericUpDown2
+            // 
+            numericUpDown2.BackColor = SystemColors.HotTrack;
+            numericUpDown2.ForeColor = SystemColors.Menu;
+            numericUpDown2.Location = new Point(295, 61);
+            numericUpDown2.Maximum = new decimal(new int[] { 55, 0, 0, 0 });
+            numericUpDown2.Name = "numericUpDown2";
+            numericUpDown2.Size = new Size(53, 23);
+            numericUpDown2.TabIndex = 7;
+            numericUpDown2.Visible = false;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(218, 105);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(130, 23);
+            textBox1.TabIndex = 8;
+            textBox1.Visible = false;
+            // 
             // Form1
             // 
             AutoScaleMode = AutoScaleMode.None;
             BackColor = SystemColors.ActiveCaption;
             BackgroundImageLayout = ImageLayout.Stretch;
-            ClientSize = new Size(482, 553);
+            ClientSize = new Size(484, 561);
+            Controls.Add(textBox1);
+            Controls.Add(numericUpDown2);
+            Controls.Add(numericUpDown1);
+            Controls.Add(button1);
             Controls.Add(groupBox1);
             ForeColor = SystemColors.GradientInactiveCaption;
             Icon = (Icon)resources.GetObject("$this.Icon");
@@ -115,9 +167,11 @@
             MinimumSize = new Size(500, 600);
             Name = "Form1";
             Text = "Clarity";
-            Load += Form1_Load;
             groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -127,5 +181,9 @@
         private Button MusikButn;
         private Button ClockButn;
         private GroupBox groupBox1;
+        private Button button1;
+        private NumericUpDown numericUpDown1;
+        private NumericUpDown numericUpDown2;
+        private TextBox textBox1;
     }
 }
--- ClarityC#/Form1.cs
+++ ClarityC#/Form1.cs
@@ -1,33 +1,66 @@
+using System.Media;
+using System.Reflection;
+
 namespace ClarityC_
 {
     public partial class Form1 : Form
     {
-        Point movestart;
+        bool ONreceipt = false;
+
+
         public Form1()
         {
             InitializeComponent();
-
-            ReceiptButn.Click += button1_Click;
+            ClockButn.Click += Clockbtn_Click;
             SettingButn.Click += button2_Click;
             MusikButn.Click += btn3_Click;
-
+            button1.Click += button1_Click_1;
         }
         private void button2_Click(object? sender, EventArgs e)
         {
-            MusikButn.Visible = !MusikButn.Visible;
+
         }
         private void btn3_Click(object? sender, EventArgs e)
         {
             this.BackColor = Color.White;
         }
-        private void button1_Click(object? sender, EventArgs e)
+        private void Clockbtn_Click(object? sender, EventArgs e)
         {
-            groupBox1.Visible = !groupBox1.Visible;
+            ONreceipt = !ONreceipt;
+            if (ONreceipt)
+            {
+                button1.Visible = true;
+                numericUpDown1.Visible = true;
+                numericUpDown2.Visible = true;
+                numericUpDown1.Text = (DateTime.Now.Hour).ToString();
+                numericUpDown2.Text = (DateTime.Now.Minute).ToString();
+                textBox1.Visible = true;
+            }
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private async void button1_Click_1(object? sender, EventArgs e)
         {
+            int taimer = 0;
+            taimer += (int)numericUpDown1.Value * 60;
+            taimer += (int)numericUpDown2.Value;
+            taimer *= 60;
+            await forWihle(taimer);
+
 
         }
+        private async Task forWihle(object taimer)
+        {
+            int taim = (int)taimer;
+
+            while (taim > 0)
+            {
+                taim--;
+                textBox1.Text = taim.ToString();   
+                await Task.Delay(1000);
+            }
+            SoundPlayer sp = new SoundPlayer("C:\\Windows\\Media\\Alarm06.wav");
+            sp.Play();
+        }
+
     }
 }
